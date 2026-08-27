26736954280abc2818295fed5acc09419abc25e2
luisOrtga <ortegaluiskb@gmail.com>
2026-08-27 15:23:32 -0600

    Update project django views

---
+++ tasks/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def signup(request):
+
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
+                user.save()
+                login(request, user)
+                return redirect("tasks")
+            except:  # noqa: E722
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Username already exists"},
+                )
+
+        return render(
+            request,
+            "signup.html",
+            {"form": UserCreationForm, "error": "Password do not match"},
+        )
+
+
+def tasks(request):
+    return render(request, "tasks.html/")
