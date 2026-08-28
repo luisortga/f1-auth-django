bee05e3eb4f9a8392bc092990e25e057f61b83f1
luisOrtga <ortegaluiskb@gmail.com>
2026-08-27 23:32:44 -0600

    Upgrade backend f1

--- tasks/views.py
+++ tasks/views.py
@@ -1,84 +1,123 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 from .forms import PilotForm
-
+from .models import Pilot
 
 # Create your views here.
-def home(request):
+
+
+def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'home.html')
 
 
-def signup(request):
-
+def signup(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        return render(request, 'signup.html', {'form': UserCreationForm})
-    else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(
-                    username=request.POST['username'],
-                    password=request.POST['password1'],
-                )
-                user.save()
-                login(request, user)
-                return redirect('pilots')
-            except IntegrityError:  # noqa: E722
-                return render(
-                    request,
-                    'signup.html',
-                    {'form': UserCreationForm, 'error': 'Username already exists'},
-                )
-
         return render(
             request,
             'signup.html',
-            {'form': UserCreationForm, 'error': 'Password do not match'},
+            {'form': UserCreationForm},
         )
 
+    if request.POST['password1'] == request.POST['password2']:
+        try:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+            )
 
-def pilots(request):
-    return render(request, 'pilots.html/')
+            user.save()
+            login(request, user)
+
+            return redirect('pilots')
+
+        except IntegrityError:
+            return render(
+                request,
+                'signup.html',
+                {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists',
+                },
+            )
+
+    return render(
+        request,
+        'signup.html',
+        {
+            'form': UserCreationForm,
+            'error': 'Password do not match',
+        },
+    )
 
 
-def create_pilot(request):
+@login_required
+def pilots(request: HttpRequest) -> HttpResponse:
 
+    pilots_all = Pilot.objects.filter(user=request.user)
+
+    return render(request, 'pilots.html', {'pilots_all': pilots_all})
+
+
+@login_required
+def create_pilot(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        return render(request, 'create_pilot.html', {'form': PilotForm})
-    else:
-        form = PilotForm(request.POST)
+        return render(
+            request,
+            'create_pilot.html',
+            {'form': PilotForm},
+        )
+
+    form = PilotForm(request.POST)
+
+    if form.is_valid():
         new_pilot = form.save(commit=False)
         new_pilot.user = request.user
         new_pilot.save()
+
         return redirect('pilots')
+    else:
+        return render(
+            request,
+            'create_pilot.html',
+            {'form': PilotForm, 'error': 'Please provide valida data'},
+        )
 
 
-def signout(request):
+def signout(request: HttpRequest) -> HttpResponse:
     logout(request)
     return redirect('home')
 
 
-def signin(request):
+def signin(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        return render(request, 'signin.html', {'form': AuthenticationForm})
-    else:
-        user = authenticate(
+        return render(
             request,
-            username=request.POST['username'],
-            password=request.POST['password'],
+            'signin.html',
+            {'form': AuthenticationForm},
         )
-        if user is None:
-            return render(
-                request,
-                'signin.html',
-                {
-                    'form': AuthenticationForm,
-                    'error': 'Username or Password is incorrect',
-                },
-            )
-        else:
-            login(request, user)
-            return redirect('pilots')
+
+    user = authenticate(
+        request,
+        username=request.POST['username'],
+        password=request.POST['password'],
+    )
+
+    if user is None:
+        return render(
+            request,
+            'signin.html',
+            {
+                'form': AuthenticationForm,
+                'error': 'Username or Password is incorrect',
+            },
+        )
+
+    login(request, user)
+
+    return redirect('pilots')
